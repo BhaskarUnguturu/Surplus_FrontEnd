70b37d694817529bdede0b03da31aead9bc7d7da
Thareesh <thareeshr@gmail.com>
2023-06-22 19:28:03 -0500

    Added the Export button in the food donation component

--- src/app/views/admin/food/food.service.ts
+++ src/app/views/admin/food/food.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { UtilityService } from '../../../shared/services/utility.service';
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { ApiService } from '../../../shared/services/api.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { LoadingService } from '../../../shared/services/loading.service';
-import { Pagination } from '../../../shared/interfaces/pagination.interface';
+
+import { ExcelService } from '../../../shared/services/excel.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class FoodService implements Resolve<any>{
-
-    onDataChanged: BehaviorSubject<any>;
-    onDataListChanged: BehaviorSubject<Pagination>;
+export class FoodService {
     data: any = null;
     routeParams: any = null;
     state: string = "";
+
+    STATUS: any[] = [
+        { key: 0, value: 'Pending', color: 'accent' },
+        { key: 1, value: 'Inprogress', color: 'warn' },
+        { key: 2, value: 'Completed', color: 'primary' }
+    ]
+
+    FOOD_TYPES: any[] = [
+        { key: 'Fruit and vegetables', value: 'Fruit and vegetables', color: 'primary' },
+        { key: 'Starchy food', value: 'Starchy food', color: 'warn' },
+        { key: 'Dairy', value: 'Dairy', color: 'warn' },
+        { key: 'Protein', value: 'Protein', color: 'warn' },
+        { key: 'Fat', value: 'Fat', color: 'warn' }
+    ]
 
     /**
      * Constructor
@@ -29,69 +39,8 @@ export class FoodService implements Resolve<any>{
         private _apiService: ApiService,
         private _formBuilder: FormBuilder,
         private _loadingService: LoadingService,
-        private _router: Router
+        private excelService: ExcelService
     ) {
-        // Set the defaults
-        this.onDataChanged = new BehaviorSubject({});
-        this.onDataListChanged = new BehaviorSubject<Pagination>(_utilityService.pagination);
-    }
-
-    /**
-      * Resolver
-      *
-      * @param {ActivatedRouteSnapshot} route
-      * @param {RouterStateSnapshot} state
-      * @returns {Observable<any> | Promise<any> | any}
-      */
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
-        this.routeParams = route.params;
-        this.state = state.url;
-
-        return new Promise((resolve, reject) => {
-
-            Promise.all([
-                this.getUserForResolve()
-            ]).then(
-                () => {
-                    resolve(0);
-                },
-                reject
-            );
-        });
-    }
-
-    getUserForResolve(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            if (this.state.includes('add')) {
-                this.data = undefined;
-                this.onDataChanged.next(false);
-                resolve(false);
-            }
-            else if (this.state.includes('edit') && this.routeParams.id) {
-                this.getDataById(this.routeParams.id).then((response: any) => {
-                    this.data = response.body.data;
-                    this.onDataChanged.next(this.data);
-                    resolve(response);
-                }, reject);
-            }
-        });
-    }
-
-    /**
-     * Create form
-     * 
-     * @param element 
-     */
-    createForm(element?: any): FormGroup {
-        return this._formBuilder.group({
-            dietaryRestrictions: [element ? element.dietaryRestrictions : null, [Validators.required]],
-            expirationDate: [element ? element.expirationDate : null, [Validators.required]],
-            quantity: [element ? element.quantity : null, [Validators.required]],
-            schedulingTimeDate: [element ? element.schedulingTimeDate : null, [Validators.required]],
-            typeOfDonation: [element ? element.typeOfDonation : null, [Validators.required]],
-            typeOfFood: [element ? element.typeOfFood : null, [Validators.required]],
-            venue: [element ? element.venue : null, [Validators.required]]
-        })
     }
 
     /**
@@ -106,19 +55,57 @@ export class FoodService implements Resolve<any>{
         })
     }
 
+    getStatus(key: any) {
+        let element = this.STATUS.find(item => item.key === key);
+        if (element) {
+            return element.value;
+        } else {
+            return '';
+        }
+    }
+
+    getStatusColor(key: any) {
+        let element = this.STATUS.find(item => item.key === key);
+        if (element) {
+            return element.color;
+        } else {
+            return '';
+        }
+    }
+
     /**
-     * Add or save data
+     * Get data by id
      * 
-     * @param data 
-     * @param type 
+     * @param id 
      */
-    addOrUpdateData(data: any, type: any) {
+    getDataById(id: any) {
+        return this._apiService.get(`food/getById/${id}`);
+    }
+
+    getDashboardData() {
+        return this._apiService.get(`dashboard`);
+    }
+
+    exportData(data: any[]) {
+        let dataList: any[] = [];
+        data.forEach(element => {
+            let json = element;
+            dataList.push(json);
+        });
+        
+        this.excelService.exportToExcel(dataList, "Foods");
+    }
+
+    getDataListByUserId(id: any) {
+        return this._apiService.get(`food/list/${id}`);
+    }
+
+    addRating(data: any) {
         this._loadingService.loading.next(true);
-        this._apiService.post(type === 'add' ? 'food/add' : 'food/update', data).then((response: any) => {
+        this._apiService.post('rating/add', data).then((response: any) => {
             if (response && response.status === 'OK') {
                 this._loadingService.loading.next(false);
                 this._utilityService.successMessage(response.message, response.status);
-                this._router.navigateByUrl("/admin/food");
             } else {
                 this._loadingService.loading.next(false);
                 this._utilityService.errorMessage(response.message, response.status);
@@ -131,76 +118,18 @@ export class FoodService implements Resolve<any>{
             } else {
                 this._utilityService.errorMessage(error.error.message, error.statusText);
             }
-        });;
+        });
     }
 
-    /**
-     * Get data list with pagination
-     * 
-     * @param data 
-     */
-    getListWithPagination(data: any) {
-        return this._apiService.post('user/getUserListWithPagination', data);
-    }
-
-    /**
-     * Get data list by filter with pagination
-     * 
-     * @param data 
-     */
-    getListByFilterWithPagination(data: any) {
+    addFeedback(data: any) {
         this._loadingService.loading.next(true);
-        this._apiService.post('user/pagination/filter', data).then((response: any) => {
-            this._loadingService.loading.next(false);
-            this.onDataListChanged.next(response);
-        }, error => {
-            this._loadingService.loading.next(false);
-            console.log(error);
-            if (error.status === 0) {
-                this._utilityService.errorMessage('Internal Server Error! Please try again after some time', 'ERROR');
+        this._apiService.post('feedback/add', data).then((response: any) => {
+            if (response && response.status === 'OK') {
+                this._loadingService.loading.next(false);
+                this._utilityService.successMessage(response.message, response.status);
             } else {
-                this._utilityService.errorMessage(error.error.message, error.statusText);
-            }
-        })
-    }
-
-    /**
-     * Get data list by filter with pagination
-     * 
-     * @param data 
-     */
-    getListByFilter(data: any) {
-        this._loadingService.loading.next(true);
-        this._apiService.post('user/getListByFilter', data).then((response: any) => {
-            this._loadingService.loading.next(false);
-            if (response && response.body.status === 'OK') {
-                this.exportData(response.body.data);
-            } else {
-                this._utilityService.errorMessage(response.body.message, response.body.status);
-            }
-        }, error => {
-            this._loadingService.loading.next(false);
-            console.log(error);
-            if (error.status === 0) {
-                this._utilityService.errorMessage('Internal Server Error! Please try again after some time', 'ERROR');
-            } else {
-                this._utilityService.errorMessage(error.error.message, error.statusText);
-            }
-        });;
-    }
-
-    /**
-     * Change data status
-     */
-    changeStatus(formData: any, json: any) {
-        this._loadingService.loading.next(true);
-        this._apiService.post(formData, `user/changeStatus`).then((response: any) => {
-            this._loadingService.loading.next(false);
-            if (response && response.statusText === 'OK') {
-                this.getListByFilterWithPagination(json);
-                this._utilityService.successMessage(response.body.message, response.status);
-            } else {
-                this._utilityService.errorMessage(response.body.message, response.status);
+                this._loadingService.loading.next(false);
+                this._utilityService.errorMessage(response.message, response.status);
             }
         }, error => {
             this._loadingService.loading.next(false);
@@ -213,107 +142,15 @@ export class FoodService implements Resolve<any>{
         });
     }
 
-    /**
-     * Get data list by filter
-     * 
-     * @param data 
-     */
-    getUserListByFilter(data: any) {
-        return this._apiService.post('user/getUserListByFilter', data);
+    getRating(id: any) {
+        return this._apiService.get(`user/${id}/ratings`);
     }
 
-    /**
-     * Check data email exist
-     * 
-     * @param emailId 
-     */
-    checkUserExistByEmail(emailId: any) {
-        return this._apiService.get(`user/checkUserExistByEmail${emailId}`);
+    getFeedback(id: any) {
+        return this._apiService.get(`user/${id}/feedback`);
     }
 
-    /**
-     * Get All Data
-     */
-    getAll() {
-        return this._apiService.get(`user/getAll`);
-    }
-
-    /**
-     * Get data by id
-     * 
-     * @param id 
-     */
-    getDataById(id: any) {
-        return this._apiService.get(`user/getById/${id}`);
-    }
-
-    /**
-     * Get user details by id
-     * 
-     * @param userId 
-     */
-    getUserDetailsById(userId: any) {
-        return this._apiService.get(`user/getUserDetailsById/${userId}`);
-    }
-
-    /**
-     * get user list
-     */
-    getUserList() {
-        return this._apiService.get('user/getUserList');
-    }
-
-    /**
-     * Delete  by id
-     * 
-     * @param id 
-     */
-    deleteById(id: string, json: any) {
-        this._loadingService.loading.next(true);
-        this._apiService.delete(`user/deleteById/${id}`).then((response: any) => {
-            this._loadingService.loading.next(false);
-            if (response && response.body.status === 'OK') {
-                this.getListByFilterWithPagination(json);
-                this._utilityService.successMessage(response.body.message, response.body.status);
-            } else {
-                this._utilityService.errorMessage(response.body.message, response.body.status);
-            }
-        }, error => {
-            this._loadingService.loading.next(false);
-            console.log(error);
-            if (error.status === 0) {
-                this._utilityService.errorMessage('Internal Server Error! Please try again after some time', 'ERROR');
-            } else {
-                this._utilityService.errorMessage(error.error.message, error.statusText);
-            }
-        });
-    }
-
-    exportData(data: any[]) {
-        let dataList: any[] = [];
-        data.forEach(element => {
-            let json = {
-                'Name': `${element.firstName} ${element.lastName}`,
-                'Email': element.email,
-                'Mobile': element.mobileNumber,
-                'Emergency Mobile': element.emergencyMobileNumber,
-                'Create Date': this._utilityService.getFormatedDateTime(element.createdAt),
-                'Date Of Birth': this._utilityService.getFormatedDate(element.dateOfBirth),
-                'Date Of Joining': this._utilityService.getFormatedDate(element.dateOfJoining),
-                'Gender': this._utilityService.getGender(element.gender),
-                'Role': this._utilityService.getUserRole(element.role),
-                'Status': this._utilityService.getStatus(element.status),
-                'Address': element.address,
-                'Country': element.country,
-                'State': element.state,
-                'City': element.city,
-                'Zip Code': element.zipCode
-            }
-            dataList.push(json);
-        });
-    }
-
-    getDataListByUserId(id: any) {
-        return this._apiService.get(`food/list/${id}`);
+    getReport() {
+        return this._apiService.get(`user/reporting`);
     }
 }
