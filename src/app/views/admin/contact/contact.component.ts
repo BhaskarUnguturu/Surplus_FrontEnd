19969f76dd95c29e2f5341803019435ea832eccf
Thareesh <thareeshr@gmail.com>
2023-06-24 16:17:36 -0500

    created a contact view component for the admin view

---
+++ src/app/views/admin/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { UtilityService } from '../../../shared/services/utility.service';
+import { ContactService } from './contact.service';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+  
+
+}
